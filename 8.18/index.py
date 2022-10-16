a5ef3050a331e9af199d47f3365e68962a23b101
Zarasfara <oev2001@gmail.com>
2022-10-16 22:39:36 +0500

    refactor: update 8.18 and update 1.34 tasks

--- 8.18/index.py
+++ 8.18/index.py
@@ -1,13 +1,22 @@
 """
 Дан символьный файл f.
-Группы слов, разделенных пробелами (одинм или несколькими) и не содержащие пробелов внутри себя, будем называть словами.
+Группы слов, разделенных пробелами (одним или несколькими) и не содержащие пробелов внутри себя, будем называть словами.
 Удалить из файла все однобуквенные слова и лишние пробелы. Результат записать в файл g.
 """
 import re
 
-document = open('f.txt', 'r').readline()
+document = open('f.txt', 'r', encoding='utf-8').readlines()
+second_document = open('g.txt', 'w', encoding='utf-8')
 
-shalom = re.sub(r'\s+', ' ', document).split(' ')
+# shalom = re.sub(r'\s+', ' ', document).split(' ')
+# document[document.index(row)] = re.sub(r'\s+', ' ', document[document.index(row)])
 
+q = []
+for row in document:
+    q += re.split(r'\s+', row)
+q.remove('')
 
-
+c = ' '.join(str(e) for e in q)
+# for x in q:
+    # if len(x) == 1:
+second_document.write(c)
